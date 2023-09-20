58d0e19006c0cfc25195e22368aa87c2dcc5045f
Jerlshin <jerlshin.official008@gmail.com>
2023-09-20 23:43:02 +0530

    IoU

---
+++ YOLO v_x Implementation/IoU.py
@@ -0,0 +1,28 @@
+''' Calculate the Intersection '''  # To calcualate the Intersection over Union 
+# we only have the number 
+# Box1 = [x1, y1, x2, y2]
+# Box2 = [x1, y1, x2, y2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
